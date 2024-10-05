df7a530abaccc2534184f60cd5dc4425ebc1b099
Vegar Ringdal <vegar.ringdal@gmail.com>
2024-10-05 21:53:53 +0200

    update dialog

--- src/components/common/SimpleGridActions.tsx
+++ src/components/common/SimpleGridActions.tsx
@@ -6,26 +6,41 @@ export function SimpleGridActions(props: {
 }) {
   return (
     <div className="flex flex-col pr-2 pt-1 gap-2 h-full">
-      <div>
-        <Button
-          pt={{
-            tooltip: {
-              root: { className: "text-xs" },
-              text: { className: "p-1" },
-            },
-          }}
-          tooltip="Toggle grid edit mode"
-          tooltipOptions={{ showDelay: 1000 }}
-          className="p-1 h-6 w-6 bg-gray-700 hover:bg-gray-600 text-gray-200 border-0"
-          onClick={() => {
-            const config = props.interface.saveConfig();
-            config.readonly = !config.readonly;
-            props.interface.loadConfig(config);
-          }}
-        >
-          <i className="pi pi-pencil"></i>
-        </Button>
-      </div>
+      <Button
+        pt={{
+          tooltip: {
+            root: { className: "text-xs" },
+            text: { className: "p-1" },
+          },
+        }}
+        tooltip="Load/refresh data"
+        tooltipOptions={{ showDelay: 1000 }}
+        className="p-1 h-6 w-6 bg-gray-700 hover:bg-gray-600 text-gray-200 border-0"
+        onClick={() => {
+          console.log(props.interface.getDatasource().getChanges());
+        }}
+      >
+        <i className="pi pi-sync"></i>
+      </Button>
+
+      <Button
+        pt={{
+          tooltip: {
+            root: { className: "text-xs" },
+            text: { className: "p-1" },
+          },
+        }}
+        tooltip="Toggle grid edit mode"
+        tooltipOptions={{ showDelay: 1000 }}
+        className="p-1 h-6 w-6 bg-gray-700 hover:bg-gray-600 text-gray-200 border-0"
+        onClick={() => {
+          const config = props.interface.saveConfig();
+          config.readonly = !config.readonly;
+          props.interface.loadConfig(config);
+        }}
+      >
+        <i className="pi pi-pencil"></i>
+      </Button>
 
       <Button
         pt={{
@@ -110,6 +125,24 @@ export function SimpleGridActions(props: {
         }}
       >
         <i className="pi pi-file-import"></i>
+      </Button>
+
+
+      <Button
+        pt={{
+          tooltip: {
+            root: { className: "text-xs" },
+            text: { className: "p-1" },
+          },
+        }}
+        tooltip="Saves selected record"
+        tooltipOptions={{ showDelay: 1000 }}
+        className="p-1 h-6 w-6 bg-gray-700 hover:bg-gray-600 text-gray-200 border-0"
+        onClick={() => {
+          alert("Not implemented");
+        }}
+      >
+        <i className="pi pi-save"></i>
       </Button>
 
       <Button
