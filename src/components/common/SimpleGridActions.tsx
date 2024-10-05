5350cd234b5a78940d2015d73dfa2550f8c43553
Vegar Ringdal <vegar.ringdal@gmail.com>
2024-10-05 21:12:50 +0200

    fix tooltip

--- src/components/common/SimpleGridActions.tsx
+++ src/components/common/SimpleGridActions.tsx
@@ -1,11 +1,22 @@
 import { GridInterface } from "@simple-html/grid";
+import { Button } from "primereact/button";
 
-export function SimpleGridActions(props: { interface: GridInterface<unknown> }) {
+export function SimpleGridActions(props: {
+  interface: GridInterface<unknown>;
+}) {
   return (
     <div className="flex flex-col pr-2 pt-1 gap-2 h-full">
       <div>
-        <button
-          className="p-1 h-6 w-6 bg-gray-700 hover:bg-gray-600"
+        <Button
+          pt={{
+            tooltip: {
+              root: { className: "text-xs" },
+              text: { className: "p-1" },
+            },
+          }}
+          tooltip="Toggle grid edit mode"
+          tooltipOptions={{ showDelay: 1000 }}
+          className="p-1 h-6 w-6 bg-gray-700 hover:bg-gray-600 text-gray-200 border-0"
           onClick={() => {
             const config = props.interface.saveConfig();
             config.readonly = !config.readonly;
@@ -13,35 +24,59 @@ export function SimpleGridActions(props: { interface: GridInterface<unknown> }) 
           }}
         >
           <i className="pi pi-pencil"></i>
-        </button>
+        </Button>
       </div>
 
-      <button
-        className="p-1 h-6 w-6 bg-gray-700 hover:bg-gray-600"
+      <Button
+        pt={{
+          tooltip: {
+            root: { className: "text-xs" },
+            text: { className: "p-1" },
+          },
+        }}
+        tooltip="Add new empty element"
+        tooltipOptions={{ showDelay: 1000 }}
+        className="p-1 h-6 w-6 bg-gray-700 hover:bg-gray-600 text-gray-200 border-0"
         onClick={() => {
           props.interface.getDatasource().addNewEmpty();
         }}
       >
         <i className="pi pi-plus"></i>
-      </button>
+      </Button>
 
-      <button
-        className="p-1 h-6 w-6 bg-gray-700 hover:bg-gray-600"
+      <Button
+        pt={{
+          tooltip: {
+            root: { className: "text-xs" },
+            text: { className: "p-1" },
+          },
+        }}
+        tooltip="Reset all edits"
+        tooltipOptions={{ showDelay: 1000 }}
+        className="p-1 h-6 w-6 bg-gray-700 hover:bg-gray-600 text-gray-200 border-0"
         onClick={() => {
           props.interface.getDatasource().resetData();
         }}
       >
         <i className="pi pi-undo"></i>
-      </button>
+      </Button>
 
-      <button
-        className="p-1 h-6 w-6 bg-gray-700 hover:bg-gray-600"
+      <Button
+        pt={{
+          tooltip: {
+            root: { className: "text-xs" },
+            text: { className: "p-1" },
+          },
+        }}
+        tooltip="Prints all edits to console"
+        tooltipOptions={{ showDelay: 1000 }}
+        className="p-1 h-6 w-6 bg-gray-700 hover:bg-gray-600 text-gray-200 border-0"
         onClick={() => {
           console.log(props.interface.getDatasource().getChanges());
         }}
       >
         <i className="pi pi-print"></i>
-      </button>
+      </Button>
     </div>
   );
 }
