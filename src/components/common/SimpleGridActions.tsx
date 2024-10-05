9913b6b6872121f4a322b734398fb403e76c77b6
Vegar Ringdal <vegar.ringdal@gmail.com>
2024-10-05 21:51:42 +0200

    more default buttons

--- src/components/common/SimpleGridActions.tsx
+++ src/components/common/SimpleGridActions.tsx
@@ -68,7 +68,58 @@ export function SimpleGridActions(props: {
             text: { className: "p-1" },
           },
         }}
-        tooltip="Prints all edits to console"
+        tooltip="Delete selected"
+        tooltipOptions={{ showDelay: 1000 }}
+        className="p-1 h-6 w-6 bg-gray-700 hover:bg-gray-600 text-gray-200 border-0"
+        onClick={() => {
+          alert("Not implemented");
+        }}
+      >
+        <i className="pi pi-trash"></i>
+      </Button>
+
+      <Button
+        pt={{
+          tooltip: {
+            root: { className: "text-xs" },
+            text: { className: "p-1" },
+          },
+        }}
+        tooltip="Duplicates current"
+        tooltipOptions={{ showDelay: 1000 }}
+        className="p-1 h-6 w-6 bg-gray-700 hover:bg-gray-600 text-gray-200 border-0"
+        onClick={() => {
+          alert("Not implemented");
+        }}
+      >
+        <i className="pi pi-copy"></i>
+      </Button>
+
+      <Button
+        pt={{
+          tooltip: {
+            root: { className: "text-xs" },
+            text: { className: "p-1" },
+          },
+        }}
+        tooltip="Open import helper, for dumping data from excel"
+        tooltipOptions={{ showDelay: 1000 }}
+        className="p-1 h-6 w-6 bg-gray-700 hover:bg-gray-600 text-gray-200 border-0"
+        onClick={() => {
+          alert("Not implemented");
+        }}
+      >
+        <i className="pi pi-file-import"></i>
+      </Button>
+
+      <Button
+        pt={{
+          tooltip: {
+            root: { className: "text-xs" },
+            text: { className: "p-1" },
+          },
+        }}
+        tooltip="Prints all edits to console (debug only)"
         tooltipOptions={{ showDelay: 1000 }}
         className="p-1 h-6 w-6 bg-gray-700 hover:bg-gray-600 text-gray-200 border-0"
         onClick={() => {
