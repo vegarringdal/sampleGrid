272bdae565b5dd5cd97b74bb5e3a4e71dc924a87
Vegar Ringdal <vegar.ringdal@gmail.com>
2024-10-05 21:47:18 +0200

    lint

--- src/components/modules/workprep/WorkprepEquipmentPanel.tsx
+++ src/components/modules/workprep/WorkprepEquipmentPanel.tsx
@@ -2,18 +2,20 @@ import { Splitter, SplitterPanel } from "primereact/splitter";
 import { WorkprepCommonPanel } from "./WorkprepCommonPanel";
 
 export function WorkprepEquipmentPanel() {
-  return   <Splitter
-  className="flex w-full h-full bg-inherit border-0"
-  pt={{
-    gutter: { className: "bg-gray-700" },
-    gutterHandler: { className: "bg-gray-600" },
-  }}
->
-  <SplitterPanel className="flex p-2" minSize={10} size={80}>
-      todo
-  </SplitterPanel>
-  <SplitterPanel className="flex  overflow-hidden" minSize={0} size={20}>
-    <WorkprepCommonPanel></WorkprepCommonPanel>
-  </SplitterPanel>
-</Splitter>
+  return (
+    <Splitter
+      className="flex w-full h-full bg-inherit border-0"
+      pt={{
+        gutter: { className: "bg-gray-700" },
+        gutterHandler: { className: "bg-gray-600" },
+      }}
+    >
+      <SplitterPanel className="flex p-2" minSize={10} size={80}>
+        todo
+      </SplitterPanel>
+      <SplitterPanel className="flex  overflow-hidden" minSize={0} size={20}>
+        <WorkprepCommonPanel></WorkprepCommonPanel>
+      </SplitterPanel>
+    </Splitter>
+  );
 }
