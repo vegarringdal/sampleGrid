c14280e420af0ac6afc74c86156fea9cfa6eb90e
Vegar Ringdal <vegar.ringdal@gmail.com>
2024-10-06 19:01:35 +0200

    save

--- src/components/modules/moduleSelector/ModuleSelector.tsx
+++ src/components/modules/moduleSelector/ModuleSelector.tsx
@@ -10,7 +10,7 @@ export function ModuleSelector() {
   const [selectedProject, setSelectedProject] = useState<{
     name: string;
     code: string;
-  }>(null);
+  }>();
   const navigate = useNavigate();
 
   const modules = [
@@ -18,64 +18,64 @@ export function ModuleSelector() {
       name: "Workpreperation",
       userGroup: "Workprep",
       desciption: "Create tag operations",
-      nav: () => {
-        navigate(`/workprep/${selectedProject.code}/cables`);
+      nav: (code: string) => {
+        navigate(`/workprep/${code}/cables`);
       },
     },
     {
       name: "Workpack/Task",
       userGroup: "Workprep/Foreman",
       desciption: "See details/list tagoperations",
-      nav: () => {
-        navigate(`/workpacktask/${selectedProject.code}/workpack`);
+      nav: (code: string) => {
+        navigate(`/workpacktask/${code}/workpack`);
       },
     },
     {
       name: "Drum",
       userGroup: "Workprep/Foreman/Drum Coordinator",
       desciption: "Create drum/cable types/ find drum for workpacks",
-      nav: () => {
-        navigate(`/drums/${selectedProject.code}/cables`);
+      nav: (code: string) => {
+        navigate(`/drums/${code}/cables`);
       },
     },
     {
       name: "Progress",
       userGroup: "Foreman",
       desciption: "Report progress on Cables / Equipment etc",
-      nav: () => {
-        navigate(`/progress/${selectedProject.code}/tagop`);
+      nav: (code: string) => {
+        navigate(`/progress/${code}/tagop`);
       },
     },
     {
       name: "Import",
       userGroup: "Workprep",
       desciption: "Import data to the system, like Cables/Equipment",
-      nav: () => {
-        navigate(`/import/${selectedProject.code}/cables`);
+      nav: (code: string) => {
+        navigate(`/import/${code}/cables`);
       },
     },
     {
       name: "Foreman",
       userGroup: "Workprep",
       desciption: "Import data to the system, like Cables/Equipment",
-      nav: () => {
-        navigate(`/foreman/${selectedProject.code}/all`);
+      nav: (code: string) => {
+        navigate(`/foreman/${code}/all`);
       },
     },
     {
       name: "Op codes",
       userGroup: "Workprep",
       desciption: "Changes to operation codes used on tag operations",
-      nav: () => {
-        navigate(`/opcodes/${selectedProject.code}/all`);
+      nav: (code: string) => {
+        navigate(`/opcodes/${code}/all`);
       },
     },
     {
       name: "Compensation codes",
       userGroup: "Workprep",
       desciption: "Changes to compensation codes used on tag operations",
-      nav: () => {
-        navigate(`/compensationcodes/${selectedProject.code}/all`);
+      nav: (code: string) => {
+        navigate(`/compensationcodes/${code}/all`);
       },
     },
   ];
@@ -97,7 +97,7 @@ export function ModuleSelector() {
             onChange={(e) => setSelectedProject(e.value)}
             options={projects}
             optionLabel="name"
-            className="w-full"
+            className="w-full text-sm"
           />
           <label htmlFor="project-select">Select a Project</label>
         </FloatLabel>
@@ -105,27 +105,31 @@ export function ModuleSelector() {
 
       <div className="m-auto w-4/5 mt-5 flex-1">
         <DataTable value={modules} size="small">
-          <Column field="name" header="Module" className="w-1/5"></Column>
+          <Column
+            field="name"
+            header="Module Name"
+            className="w-1/5 text-sm"
+          ></Column>
           <Column
             field="userGroup"
-            header="User group"
-            className="w-1/5"
+            header="Module User Group"
+            className="w-1/5 text-sm"
           ></Column>
           <Column
             field="desciption"
-            header="Short desciption"
-            className="w-2/5"
+            header="About module"
+            className="w-2/5 text-sm"
           ></Column>
           <Column
             header=""
-            className="w-1/5"
+            className="w-1/5 text-sm"
             body={(rowData) => {
               return (
                 <Button
                   title="Open"
-                  className="bg-indigo-500 p-1"
+                  className="p-1 bg-gray-700 hover:bg-gray-600 text-gray-200 border-0 text-sm"
                   disabled={!selectedProject}
-                  onClick={() => rowData.nav()}
+                  onClick={() => rowData.nav(selectedProject?.code)}
                 >
                   Open
                 </Button>
