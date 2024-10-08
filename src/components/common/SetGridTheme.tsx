a96f7a0dcd8996f8d8e2a899362edeaee8435c10
Vegar Ringdal <vegar.ringdal@gmail.com>
2024-10-08 22:50:24 +0200

    align styles more

--- src/components/common/SetGridTheme.tsx
+++ src/components/common/SetGridTheme.tsx
@@ -35,14 +35,26 @@ export function SetGridTheme() {
                         --simple-html-grid-main-bg-odd: #6b7178;
                         --simple-html-grid-sec-bg-border: #1f2937;
                         --simple-html-grid-pinned-border: #1f2937;
-                        --simple-html-grid-main-bg-selected-odd: #234882;
-                        --simple-html-grid-main-bg-selected-even: #274e8f;
+                        --simple-html-grid-main-bg-selected-odd: rgb(59 130 246);
+                        --simple-html-grid-main-bg-selected-even: rgb(59 130 247);
                         --simple-html-grid-main-font-color: #f9f7f7;
                         --simple-html-grid-sec-font-color: #979494;
                         --simple-html-grid-dropzone-color: rgb(151, 148, 148, 0.4);
                         --simple-html-grid-grouping-border: #1f2937;
                         --simple-html-grid-boxshadow: #4b5563;
                         --simple-html-grid-main-hr-border: #4b5563;
+                    }
+                    .simple-html-grid .simple-html-grid-selected-row-even input {
+                        color: black;
+                    }
+                    .simple-html-grid .simple-html-grid-selected-row-odd input {
+                        color: black;
+                    }
+                    .simple-html-grid .simple-html-label-even {
+                        color: black;
+                    }
+                    .simple-html-grid .simple-html-label-odd {
+                        color: black;
                     }
                     .simple-html-grid ul.dialog-row {
                         box-shadow: none;
@@ -121,7 +133,26 @@ export function SetGridTheme() {
          */
         ref.current.append(
           document.createTextNode(`
-                    body,
+                     body,
+                    .simple-html-grid-menu,
+                    .simple-html-grid {
+                     
+                        --simple-html-grid-main-bg-selected-odd: rgb(59 130 246);
+                        --simple-html-grid-main-bg-selected-even: rgb(59 130 226);
+                      
+                    }
+                    .simple-html-grid .simple-html-grid-selected-row-even input {
+                        color: white;
+                    }
+                    .simple-html-grid .simple-html-grid-selected-row-odd input {
+                        color: white;
+                    }
+                    .simple-html-grid .simple-html-label-even {
+                        color: white;
+                    }
+                    .simple-html-grid .simple-html-label-odd {
+                        color: white;
+                    }
                     .simple-html-grid-header input::placeholder {
                         filter: opacity(1);
                     }
@@ -152,6 +183,14 @@ export function SetGridTheme() {
     
                     .simple-html-grid .filter-editor-content .grid-button {
                         color: #4f46e5;
+                    }
+
+                    .simple-html-grid .simple-html-grid-menu .simple-html-grid-menu-item-selected {
+                        color: white;
+                    }
+
+                    .simple-html-grid .simple-html-grid-menu-item:hover {
+                        color: white;
                     }
 
                     
