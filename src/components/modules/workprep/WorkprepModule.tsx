0d98ea9bdd618ecfe2b4b50080ef2845cab69b92
Vegar Ringdal <vegar.ringdal@gmail.com>
2024-10-17 20:33:59 +0200

    test how tab title can be and routing

--- src/components/modules/workprep/WorkprepModule.tsx
+++ src/components/modules/workprep/WorkprepModule.tsx
@@ -1,18 +1,39 @@
 import { TabView, TabPanel } from "primereact/tabview";
 import { WorkprepCabelsPanel } from "./WorkprepCabelsPanel";
 import { WorkprepEquipmentPanel } from "./WorkprepEquipmentPanel";
+import { useParams, useNavigate, generatePath } from "react-router";
 
 export function WorkprepModule() {
+  const navigate = useNavigate();
+  const { project, tab } = useParams();
+
+
+  // for setting tab index
+  const tabName = tab || "cables";
+  const index = tabName === "cables" ? 0 : 1; 
+
+  // poc to see how it looks
+  document.title = `PTOC-${project}-${tab}`.toUpperCase()
+
+
   return (
     <div className="flex flex-col h-full">
- 
       <TabView
+        activeIndex={index}
         pt={{
           panelContainer: {
             className: "h-full",
-          }
+          },
         }}
         className="flex flex-col flex-1 text-xs"
+        onTabChange={(e) => {
+          // so url stays correct
+          const path = generatePath("/workprep/:project/:tab", {
+            project: project as string,
+            tab: e.index === 0 ? "cables" : "equipment",
+          });
+          navigate(path);
+         }}
       >
         <TabPanel
           header="Cables"
